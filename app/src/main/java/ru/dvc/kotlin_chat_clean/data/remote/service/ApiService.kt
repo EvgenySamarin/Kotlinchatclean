597b65b171890be17306e93d00f824708f0c0eb0
Archi39 <ey.samarin@gmail.com>
2019-08-30 12:03:48 +0300

    Remote layer. Added: login, update token
    Took 20 minutes

--- app/src/main/java/ru/dvc/kotlin_chat_clean/data/remote/service/ApiService.kt
+++ app/src/main/java/ru/dvc/kotlin_chat_clean/data/remote/service/ApiService.kt
@@ -2,6 +2,7 @@ package ru.dvc.kotlin_chat_clean.data.remote.service
 
 import retrofit2.Call
 import retrofit2.http.*
+import ru.dvc.kotlin_chat_clean.data.remote.account.AuthResponse
 import ru.dvc.kotlin_chat_clean.data.remote.core.BaseResponse
 
 
@@ -17,6 +18,8 @@ interface ApiService {
     companion object {
         //methods
         const val REGISTER = "register.php"
+        const val LOGIN = "login.php"
+        const val UPDATE_TOKEN = "updateUserToken.php"
 
         //params
         const val PARAM_EMAIL = "email"
@@ -24,9 +27,20 @@ interface ApiService {
         const val PARAM_NAME = "name"
         const val PARAM_TOKEN = "token"
         const val PARAM_USER_DATE = "user_date"
+        const val PARAM_USER_ID = "user_id"
+        const val PARAM_OLD_TOKEN = "old_token"
     }
 
     @FormUrlEncoded
     @POST(REGISTER)
     fun register(@FieldMap params: Map<String, String>): Call<BaseResponse>
+
+    @FormUrlEncoded
+    @POST(LOGIN)
+    fun login(@FieldMap params: Map<String, String>): Call<AuthResponse>
+
+    @FormUrlEncoded
+    @POST(UPDATE_TOKEN)
+    fun updateToken(@FieldMap params: Map<String, String>): Call<BaseResponse>
+
 }
